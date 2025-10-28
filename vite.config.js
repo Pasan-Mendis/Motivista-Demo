c2231b71a594adb489e531cf58e5acf239edb35b
unknown <mendispasan@gmail.com>
2025-10-28 18:57:12 +0530

    script change gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import ghPages from 'vite-plugin-gh-pages';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// ESM-compatible __dirname
+// Get __dirname equivalent in ESM
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Motivista-Demo/', // matches your GitHub repo name
-  plugins: [
-    react(),
-    tailwindcss(),
-    ghPages(), // adds deploy command support
-  ],
+  plugins: [react(),tailwindcss()],
+  base: '/Motivista-Demo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      // 'components': path.resolve(__dirname, './src/components'),
+      // 'assets': path.resolve(__dirname, './src/assets'),
+      // 'styles': path.resolve(__dirname, './src/styles'),
     },
   },
-});
+})
